2122fddc4a4cf2cc33fe13c2da81260f8cf4d981
AndreHGN <andrehidekinishimura@gmail.com>
2024-02-02 12:11:53 -0300

    setup/first commit

---
+++ webpack.config.ts
@@ -0,0 +1,48 @@
+import path from 'path';
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
+import type { Configuration } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import EsLintPlugin from 'eslint-webpack-plugin';
+
+type ConfigurationAndDevServer = Configuration & { devServer: DevServerConfiguration };
+
+const config: ConfigurationAndDevServer = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'QR Challenge',
+      filename: 'index.html',
+      template: 'public/index.html',
+    }),
+    new EsLintPlugin(),
+  ],
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+};
+
+export default config;
